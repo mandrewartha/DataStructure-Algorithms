374b19ff0d9cc93100b5a55e6716c5f19fc2881b
Meghan Andrewartha <84876493+mandrewartha@users.noreply.github.com>
2021-11-15 14:10:05 -0800

    adding comments

--- camelCase/camelCase.js
+++ camelCase/camelCase.js
@@ -2,18 +2,24 @@
 
 var camelCase = function (str) {
     var result = "";
+    //split string on words and make sure all letters are lowercase
     var words = str.toLowerCase().split(" ");
 
+    //iterate through and capitalize first letter or all but the first word
     for (var i = 0; i < words.length; i++) {
         var word = words[i];
+                //split word into letters top capitalize first one
         var letters = word.split("");
 
+           //if first letter, capitalize
         if (i > 0) {
             letters[0] = letters[0].toUpperCase();
         }
 
+         //join string back together with no space
         result += letters.join("");
     }
 
+        //return string
     return result;
 };
