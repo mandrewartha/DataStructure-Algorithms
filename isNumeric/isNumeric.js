f2d4b42047a1ebcf3fa2261740b2093eb74efdef
Meghan Andrewartha <84876493+mandrewartha@users.noreply.github.com>
2021-11-16 08:06:03 -0800

    changes

--- isNumeric/isNumeric.js
+++ isNumeric/isNumeric.js
@@ -1,6 +1,7 @@
 // Write code to create a function that accepts a string and determines if the string is a valid number. You may not use any regex, built-in methods type conversion to accomplish this.
 
 var isNumeric = function (str) {
+    //create variable for all numerical digits
     var digits = {
         0: true,
         1: true,
@@ -14,23 +15,29 @@ var isNumeric = function (str) {
         9: true
     };
 
+    //set has number and has decimal to false
     var hasNum = false;
     var hasDecimal = false;
 
+    //iterate through the given string
     for (var i = 0; i < str.length; i++) {
+        //characters is equal to every index in the string
         var char = str[i];
 
+        //if the digits and characters are the same then return true
         if (digits[char]) {
             hasNum = true;
             continue;
         }
 
+        // if not a number or char continue
         if (i === 0) {
             if (char === "-" || char === "+") {
                 continue;
             }
         }
 
+        // if runs into a period, then has decimal is tru
         if (char === ".") {
             if (hasDecimal === true) {
                 return false;
