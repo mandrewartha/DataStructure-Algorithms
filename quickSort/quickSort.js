2d1973b11546179cbdfc4f7004e40c61e542376c
Meghan Andrewartha <84876493+mandrewartha@users.noreply.github.com>
2021-11-15 18:00:34 -0800

    adding comments

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -8,12 +8,14 @@ for (let i = 0; i < 2000; i++) {
 const quickSort = (array) => {
     // if input array is empty, it doesn't need to be sorted, so return it
     // this must be in place, or the recursive function calls will never end
+    // if no return then it will run forever
     if (array.length <= 1) {
         return array;
     }
 
-    // use `.slice()` to pull a single element out of the input array at random
+    // use `.splice()` to pull a single element out of the input array at random
     // this will become our pivot value, meaning we will attempt to sort the array based on a value being greater than or less than this value
+    // splitting the array on a random index
     const pivot = array.splice(Math.floor(Math.random() * array.length), 1);
 
     // we create two empty arrays, one to be populated with all the values less than or equal to the pivot value (left), and the other to be populated with all of the values greater than the pivot (right)
@@ -30,9 +32,12 @@ const quickSort = (array) => {
     });
 
     // since the array will likely not be sorted the first time going through this, we recursively call the `quickSort()` function on the `left` and `right` arrays, which will then run through this process over and over again until the input array is less than `1`...indicating the initial array has been sorted and we can return it out of the function
+    // will keep running until sorted
     return quickSort(left).concat(pivot, quickSort(right));
 };
 
+//creates the sorted array
 const sorted = quickSort(unsortedInputArray);
+// joins the two arrays together
 console.log('Post Sort:', sorted.join(' '));
 console.log('DONE!');
